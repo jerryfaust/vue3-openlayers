bf89a6e306ca336a8e19ad337498d8b1e0b91ee9
Jerry Faust <jerryfaust1@gmail.com>
2023-01-14 12:03:27 -0700

    Add support for built-in methods and events

--- src/components/mapControls/ContextMenuControl.vue
+++ src/components/mapControls/ContextMenuControl.vue
@@ -12,12 +12,41 @@ import useControl from '@/composables/useControl'
 
 export default {
     name: 'ol-context-menu',
+    // expose the built-in events
+    emits: ['beforeopen', 'open', 'close', 'add-menu-entry'],
     setup(props,context) {
+        // methods existing in the ol-contextmenu control
+        // that should be reachable via ol-context-menu
+        const clear = () => control.value.clear();
+        const close = () => control.value.close();
+        const extend = (arr) => control.value.extend(arr);
+        const push = (item) => control.value.push(item);
+        const shift = () => control.value.shift();
+        const pop = () => control.value.pop();
+        const getDefaultItems = () => control.value.getDefaultItems();
+        const isOpen = () => control.value.isOpen();
+        const updatePosition = (pixel) => control.value.updatePosition(pixel);
         const {
             control
-        } = useControl(ContextMenu, props,context);
+        } = useControl(ContextMenu, props, context);
+
+        // forward events from the ol-contextmenu control
+        control.value.on("beforeopen", (event) => {
+            context.emit('beforeopen', event)
+        })
+        control.value.on("open", (event) => {
+            context.emit('open', event)
+        })
+        control.value.on("close", (event) => {
+            context.emit('close', event)
+        })
+        control.value.on("add-menu-entry", (event) => {
+            context.emit('add-menu-entry', event)
+        })
+
         return {
-            control
+            control,
+            clear, close, extend, push, shift, pop, getDefaultItems, isOpen, updatePosition
         }
 
     },
